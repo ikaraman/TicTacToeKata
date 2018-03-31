892e399ce49c7dbe0218d0923c218b26dc8ca490
Ivan Karaman <ivan.karaman@myob.com>
2018-03-31 20:00:47 +1300

    More functions separation + more tests.

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,17 +1,56 @@
 "use strict";
 
+///////////////////////////
+const parameters = {
+    "fieldSize": 3
+};
+ticTacToe(parameters);
+///////////////////////////
+
+
 function ticTacToe(parameters) {
-    if (parameters === undefined || parameters.fieldSize !== 3) {
+    if(!validateGameParameters(parameters)){
         return false;
     }
+    const gameField = createGameField(parameters);
 
-    console.log("Let the game begin!");
+    // while (!isGameFinished(gameField)){
+        //drawTheField(gameField)
+        //makeNewTurn(gameField)
+    // }
 
     return true;
 }
 
+function validateGameParameters(parameters) {
+    if (parameters === undefined || parameters.fieldSize !== 3) {
+        console.log("Game parameters are incorrect, exiting...");
+        return false;
+    }
 
+    console.log("Game parameters are correct, let the game begin!");
+    return true;
+}
+
+function createGameField(parameters) {
+    const gameField = [];
+
+    for (let x = 1; x <= parameters.fieldSize; x++){
+        for (let y = 1; y <= parameters.fieldSize; y++){
+            gameField.push(
+                {
+                    "coordX": x,
+                    "coordY": y,
+                    "cellState": "."
+                });
+        }
+    }
+
+    return gameField;
+}
 
 module.exports = {
-    ticTacToe: ticTacToe
+    ticTacToe: ticTacToe,
+    validateGameParameters: validateGameParameters,
+    createGameField: createGameField
 };
--- tests/TicTacToe.test.js
+++ tests/TicTacToe.test.js
@@ -2,24 +2,92 @@
 
 const game = require("ticTacToe");
 
-describe("TicTacToe", () => {
-    it("should return true when called with a parameters object", () => {
+describe("validateGameParameters() function", () => {
+    it("should return true when called " +
+        "with a parameters object" +
+        "with a valid field size key", () => {
+
         let parameters = {
             "fieldSize": 3
         };
 
-        expect(game.ticTacToe(parameters)).toBe(true);
+        expect(game.validateGameParameters(parameters)).toBe(true);
     });
 
-    it("should return false when called WITHOUT a parameters object", () => {
-        expect(game.ticTacToe()).toBe(false);
+    it("should return false when called without a parameters object", () => {
+        expect(game.validateGameParameters()).toBe(false);
     });
 
-    it("should return false when called with a wrong field size in a parameters object", () => {
+    it("should return false when called with a wrong field size in a parameters object (!= 3)", () => {
         let parameters = {
             "fieldSize": 2
         };
 
-        expect(game.ticTacToe(parameters)).toBe(false);
+        expect(game.validateGameParameters(parameters)).toBe(false);
+    });
+
+
+    it("should return false when called with a incorrect data type in a parameters object (string '3')", () => {
+        let parameters = {
+            "fieldSize": "3"
+        };
+
+        expect(game.validateGameParameters(parameters)).toBe(false);
+    });
+
+
+    it("should return false when called with a wrong field size in a parameters object (null)", () => {
+        let parameters = {
+            "fieldSize": null
+        };
+
+        expect(game.validateGameParameters(parameters)).toBe(false);
+    });
+
+
+    it("should return false when called with a wrong field size in a parameters object (boolean)", () => {
+        const parameters = {
+            "fieldSize": true
+        };
+
+        expect(game.validateGameParameters(parameters)).toBe(false);
+    });
+});
+
+
+describe("createGameField() function", () => {
+    it("should return true when called ", () => {
+        const parameters = {
+            "fieldSize": 3
+        };
+
+        const expectedField = [
+                {"coordX":1,"coordY":1,"cellState":"."},
+                {"coordX":1,"coordY":2,"cellState":"."},
+                {"coordX":1,"coordY":3,"cellState":"."},
+                {"coordX":2,"coordY":1,"cellState":"."},
+                {"coordX":2,"coordY":2,"cellState":"."},
+                {"coordX":2,"coordY":3,"cellState":"."},
+                {"coordX":3,"coordY":1,"cellState":"."},
+                {"coordX":3,"coordY":2,"cellState":"."},
+                {"coordX":3,"coordY":3,"cellState":"."}
+            ];
+
+        expect(game.createGameField(parameters)).toEqual(expectedField);
+    });
+});
+
+
+describe("ticTacToe() function", () => {
+    it("should stop execution when game parameters are invalid", () => {
+        expect(game.ticTacToe("sdfd")).toBe(false);
+    });
+
+    it("should return true when called with valid game parameters", () => {
+        const parameters = {
+            "fieldSize": 3
+        };
+
+        expect(game.ticTacToe(parameters)).toBe(true);
     });
 });
